35c1a9c1bfd0ad702ba02d196cf0e0801a3bc2b5
yashraj-m <yashrajmaheshwari15@gmail.com>
2021-11-09 21:47:50 +0530

    home page added customers stories

--- home.js
+++ home.js
@@ -88,3 +88,34 @@ for(var i = 0; i < myvideo.length; i++) {
         this.play();
     })
 }
+
+document.querySelector(".next").addEventListener("click", ()=> {
+    plusDivs(1)
+})
+
+document.querySelector(".previous").addEventListener("click", ()=> {
+    plusDivs(-1)
+})
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("class2");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  console.log(x, n);
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "flex";
+}
